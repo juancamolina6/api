aba42fe33423a1adf181722018640d959f4e4107
juan camilo <juanca2.0@hotmail.com>
2022-03-17 21:30:00 -0500

    agregando base de datos externa

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ from flask_cors import CORS
 # conexion a db
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root@localhost/flaskmysql'
-# app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://sql10478294:dXuQMgmMfx@sql10.freesqldatabase.com:3306/sql10478294'
+# app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://root@localhost/flaskmysql'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://sql10478294:dXuQMgmMfx@sql10.freesqldatabase.com:3306/sql10478294'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
